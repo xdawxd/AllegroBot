440a6b14eb2a55d0aad204b7e552627684ed9c51
xdawxd <xrealz110@gmail.com>
2020-11-09 19:11:01 +0100

    Update AllegroBot.py

--- AllegroBot.py
+++ AllegroBot.py
@@ -37,97 +37,97 @@ soup = BeautifulSoup(page.text, features='lxml')
 item_container = soup.findAll('div', {'class': 'mpof_ki myre_zn _9c44d_1Hxbq'})
 
 # Statement that checks if any items are found.
-if not len(item_container) < 1:
+if len(item_container) != 0:
     if not os.path.exists(path):
         print(f'Creating folder {os.path.basename(path)}...')
         os.mkdir(path)
     os.chdir(path)
-    time.sleep(1)
+else:
+    print('No items match your search.')
+    quit()
 
-    filename = f"allegro_{search_for.replace(' ', '_')}.txt"
+time.sleep(1)
+filename = f"allegro_{search_for.replace(' ', '_')}.txt"
 
-    # If the file already exists, user can decide if he wants to delete it or not.
-    if os.path.exists(os.path.join(path, filename)):
-        f_exists = re.compile(r'(y.*|sure|ok|okay|alright)', re.I)
-        if_delete = input(
-            'There is already a file with the same name do you want to delete it? ')
+# If the file already exists, user can decide if he wants to delete it or not.
+if os.path.exists(os.path.join(path, filename)):
+    f_exists = re.compile(r'(y.*|sure|ok|okay|alright)', re.I)
+    if_delete = input(
+        'There is already a file with the same name do you want to delete it? ')
 
-        if f_exists.search(if_delete):
-            print('Deleting...')
-            time.sleep(1)
-            for file in os.listdir(os.getcwd()):
-                logging.shutdown()
-                file_path = os.path.join(os.getcwd(), file)
-                try:
-                    # Used send2trash package which speaks for its name and sends the files to the Recycle Bin.
-                    send2trash.send2trash(file_path)
-                except Exception as e:
-                    print('Failed to delete %s. Reason: %s' % (file_path, e))
-        else:
-            quit()
+    if f_exists.search(if_delete):
+        print('Deleting...')
+        time.sleep(1)
+        for file in os.listdir(os.getcwd()):
+            logging.shutdown()
+            file_path = os.path.join(os.getcwd(), file)
+            try:
+                # Used send2trash package which speaks for its name and sends the files to the Recycle Bin.
+                send2trash.send2trash(file_path)
+            except Exception as e:
+                print('Failed to delete %s. Reason: %s' % (file_path, e))
+    else:
+        quit()
 
-    print('Creating file: AllegroBot.log...')
-    time.sleep(1)
-    # Creating a logging file which saves all errors that the program ran into.
-    logging.basicConfig(filename='AllegroBot.log', level=logging.INFO, format='%(asctime)s - '
+print('Creating file: AllegroBot.log...')
+time.sleep(1)
+# Creating a logging file which saves all errors that the program ran into.
+logging.basicConfig(filename='AllegroBot.log', level=logging.INFO, format='%(asctime)s - '
                                                                               '%(levelname)s - '
                                                                               '%(''message)s')
 
-    for i in range(1, len(item_container)):
+for i in range(1, len(item_container)):
+    try:
+        price = convert_price_to_number(item_container[i].find(
+            'span', class_='_1svub _lf05o').text[:-3])
+
+        # Variables that contain specific information about a product.
+        name = item_container[i].find(
+            'a', class_='_w7z6o _uj8z7 meqh_en mpof_z0 mqu1_16 _9c44d_2vTdY').text
+        link = item_container[i].find(
+            'a', class_='_w7z6o _uj8z7 meqh_en mpof_z0 mqu1_16 _9c44d_2vTdY').get('href')
+
+        # List of Tuples that contain additional info about the product, e.g. ('matrix': '30 inches').
+        info = list(zip([item_1.text for item_1 in item_container[i].find_all('dt')],
+                        [item_2.text for item_2 in item_container[i].find_all('dd')]))
+
         try:
-            price = convert_price_to_number(item_container[i].find(
-                'span', class_='_1svub _lf05o').text[:-3])
+            percent = item_container[i].find('span', class_='_9c44d_1uHr2').text
+            discount = convert_price_to_number(item_container[i].find(
+                'span', class_='mpof_uk mqu1_ae _9c44d_18kEF m9qz_yp _9c44d_2BSa0 _9c44d_KrRuv').text[:-3])
 
-            # Variables that contain specific information about a product.
-            name = item_container[i].find(
-                'a', class_='_w7z6o _uj8z7 meqh_en mpof_z0 mqu1_16 _9c44d_2vTdY').text
-            link = item_container[i].find(
-                'a', class_='_w7z6o _uj8z7 meqh_en mpof_z0 mqu1_16 _9c44d_2vTdY').get('href')
+            data = f"Counter: {i}\nName: {name}\nPrice: Discounted by {percent[1:]} from {discount} to {price}" \
+                    f"\nAdditional info: {str(info)[1:-1]}\nLink: {link}"
 
-            # List of Tuples that contain additional info about the product, e.g. ('matrix': '30 inches').
-            info = list(zip([item_1.text for item_1 in item_container[i].find_all('dt')],
-                            [item_2.text for item_2 in item_container[i].find_all('dd')]))
-
+            # If the '-p' is passed into the terminal
+            # Append only cheaper or the same price as passed
             try:
-                percent = item_container[i].find('span', class_='_9c44d_1uHr2').text
-                discount = convert_price_to_number(item_container[i].find(
-                    'span', class_='mpof_uk mqu1_ae _9c44d_18kEF m9qz_yp _9c44d_2BSa0 _9c44d_KrRuv').text[:-3])
-
-                data = f"Counter: {i}\nName: {name}\nPrice: Discounted by {percent[1:]} from {discount} to {price}" \
-                       f"\nAdditional info: {str(info)[1:-1]}\nLink: {link}"
-
-                # If the '-p' is passed into the terminal
-                # Append only cheaper or the same price as passed
-                try:
-                    if price <= price_below:
-                        item_list.append(data)
-                except Exception:
+                if price <= price_below:
                     item_list.append(data)
-
             except Exception:
-                item_list.append(f"Counter: {i}\nName: {name}\nPrice: {price}\nAdditional info: {str(info)[1:-1]}"
+                item_list.append(data)
+
+        except Exception:
+            item_list.append(f"Counter: {i}\nName: {name}\nPrice: {price}\nAdditional info: {str(info)[1:-1]}"
                                  f"\nLink: {link}")
 
-        # The class passed in the if statement is a class for sponsored items which we don't want
-        # So whenever the program runs into that div it skips it and logs the information to the .log file
-        except AttributeError as exc:
-            if soup.findAll('div', {'class': '_1y62o mpof_ki _9c44d_3SD3k'}):
-                logging.info(
-                    'The program found a sponsored item and skipped it.')
-        except Exception as exc:
-            logging.info(exc)
+    # The class passed in the if statement is a class for sponsored items which we don't want
+    # So whenever the program runs into that div it skips it and logs the information to the .log file
+    except AttributeError as exc:
+        if soup.findAll('div', {'class': '_1y62o mpof_ki _9c44d_3SD3k'}):
+            logging.info(
+                'The program found a sponsored item and skipped it.')
+    except Exception as exc:
+        logging.info(exc)
 
-    # Creating a .txt file for the results.
-    time.sleep(1)
-    try:
-        with open(filename, 'w', encoding='utf-8') as file:
-            for item in item_list:
-                file.write(item + os.linesep)
-            print(f"Creating file: {filename}...")
-            time.sleep(1)
-    except FileNotFoundError as exc:
-        logging.critical(exc)
-    print('Done!')
-
-else:
-    print('No items match your search.')
+# Creating a .txt file for the results.
+time.sleep(1)
+try:
+    with open(filename, 'w', encoding='utf-8') as file:
+        for item in item_list:
+            file.write(item + os.linesep)
+        print(f"Creating file: {filename}...")
+        time.sleep(1)
+except FileNotFoundError as exc:
+    logging.critical(exc)
+print('Done!')
