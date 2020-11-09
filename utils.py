b284016f0136ce0553daebb382cc1de0447e0590
xdawxd <xrealz110@gmail.com>
2020-11-09 18:33:13 +0100

    Added utils.py

---
+++ utils.py
@@ -0,0 +1,17 @@
+def convert_price_to_number(price):
+    """
+    Converts for example a string of 1 122,99
+     to a float of 1122.99
+    """
+    import re
+
+    rep = {',': '.', ' ': ''}
+    rep = dict((re.escape(k), v) for k, v in rep.items())
+
+    pattern = re.compile('|'.join(rep.keys()))
+    return float(pattern.sub(lambda m: rep[re.escape(m.group(0))], price))
+
+
+discount = convert_price_to_number('499,00 zł')
+
+print(discount)
