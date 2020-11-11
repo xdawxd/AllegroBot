81db20254b31d626888c43c93b8d9bf6cc0b3dce
Dawid Schwinge <58686770+xdawxd@users.noreply.github.com>
2020-11-11 22:55:09 +0100

    Update utils.py

--- utils.py
+++ utils.py
@@ -1,10 +1,11 @@
+import re
+
+
 def convert_price_to_number(price: str):
     """
     Converts for example a string of 1 122,99
      to a float of 1122.99.
-    """
-    import re
-
+    """ 
     rep = {',': '.', ' ': ''}
     rep = dict((re.escape(k), v) for k, v in rep.items())
     pattern = re.compile('|'.join(rep.keys()))
@@ -16,8 +17,6 @@ def value_strip(tag: str, searched: str):
     """
     Strips a tag off a value and returns it.
     """
-    import re
-
     try:
         strip_re = re.compile(rf'(-{tag}\s*)(\d*)')
         value = float(strip_re.search(str(searched)).group(2))
@@ -32,8 +31,6 @@ def search_strip(searched: str):
     """
     Strips a tag and a value off a search and returns it.
     """
-    import re
-
     strip_re = re.findall(r'(-p\s*)(\d*)', searched, re.I)
     temp = [''.join(''.join(j)) for j in [i for i in strip_re]]
 
