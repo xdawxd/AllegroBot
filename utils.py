fd0cf95081b576862c57f07d6fa6212249d977d1
xdawxd <xrealz110@gmail.com>
2020-12-01 12:51:10 +0100

    Improved readability of the code.

--- utils.py
+++ utils.py
@@ -2,11 +2,6 @@ import re
 
 
 def convert_price_to_number(price: str):
-    """
-    Converts for example a string of 1 122,99
-     to a float of 1122.99.
-    """
-
     rep = {',': '.', ' ': ''}
     rep = dict((re.escape(k), v) for k, v in rep.items())
 
@@ -15,14 +10,9 @@ def convert_price_to_number(price: str):
 
 
 def value_strip(tag: str, searched: str):
-    """
-    Strips a tag off a value and returns it.
-    """
-
     try:
         strip_re = re.compile(rf'(-{tag}\s*)(\d*)')
         value = float(strip_re.search(str(searched)).group(2))
-
         return value
 
     except AttributeError:
@@ -30,10 +20,6 @@ def value_strip(tag: str, searched: str):
 
 
 def search_strip(searched: str):
-    """
-    Strips a tag and a value off a search and returns it.
-    """
-
     strip_re = re.findall(r'(-p\s*)(\d*)', searched, re.I)
 
     temp = [''.join(''.join(j)) for j in [i for i in strip_re]]
