fdf5cde064e47a69c2c7af3c1a099d83d336a655
xdawxd <xrealz110@gmail.com>
2020-12-01 12:54:56 +0100

    Improved readability of the code.

--- utils.py
+++ utils.py
@@ -1,11 +1,10 @@
 import re
 
-
 def convert_price_to_number(price: str):
     rep = {',': '.', ' ': ''}
     rep = dict((re.escape(k), v) for k, v in rep.items())
-
     pattern = re.compile('|'.join(rep.keys()))
+    
     return float(pattern.sub(lambda m: rep[re.escape(m.group(0))], price))
 
 
@@ -21,7 +20,6 @@ def value_strip(tag: str, searched: str):
 
 def search_strip(searched: str):
     strip_re = re.findall(r'(-p\s*)(\d*)', searched, re.I)
-
     temp = [''.join(''.join(j)) for j in [i for i in strip_re]]
 
     return searched.strip(' '.join(temp))
